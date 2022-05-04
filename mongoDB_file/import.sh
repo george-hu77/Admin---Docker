cba1ff9148207ed4dfe64a7a00fe3eb350919f84
george-hu77 <55352261+george-hu77@users.noreply.github.com>
2022-05-03 20:43:49 -0400

    Update import.sh

--- mongoDB_file/import.sh
+++ mongoDB_file/import.sh
@@ -3,5 +3,4 @@
 mongoimport --uri "mongodb://yourusername:yourpassword@localhost:27017/react-app?authSource=admin" --collection users --drop --file users.json --jsonArray;
 mongoimport --uri "mongodb://yourusername:yourpassword@localhost:27017/react-app?authSource=admin" --collection products --drop --file products.json --jsonArray;
 mongoimport --uri "mongodb://yourusername:yourpassword@localhost:27017/react-app?authSource=admin" --collection roles --drop --file roles.json --jsonArray;
-mongoimport --uri "mongodb://yourusername:yourpassword@localhost:27017/react-app?authSource=admin" --collection categories --drop --file categories.json --jsonArray;
-mongoimport --uri "mongodb://yourusername:yourpassword@localhost:27017/react-app?authSource=admin" --collection work --drop --file work.json --jsonArray;
+mongoimport --uri "mongodb://yourusername:yourpassword@localhost:27017/react-app?authSource=admin" --collection categorys --drop --file categories.json --jsonArray;
